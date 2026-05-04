c23cb4257f0e1407bbcea3b9fd6d1c1b4fa35ca6
Sjoerd Bolten <sjoerdabolten@gmail.com>
2026-05-04 21:54:27 +0200

    chore(ios): import blank Xcode project
    SwiftUI single-target app, signing configured for Apple Developer team
    FCB4S3W235, bundle id dev.sjoerd.tringtring. To be expanded in build
    step 8 with APNs registration, BackendClient, and webhook URL UI.

---
+++ ios/Tring Tring/Tring_TringApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tring_TringApp.swift
+//  Tring Tring
+//
+//  Created by Sjoerd Bolten on 04/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct Tring_TringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
